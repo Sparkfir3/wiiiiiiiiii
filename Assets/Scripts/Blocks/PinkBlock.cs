ac0f920938db5af0ee46a7ae3d11c005ea063e62
Bill Dang <sparkfire.ffs@gmail.com>
2021-01-16 13:54:32 -0600

    Added light green and pink blocks

---
+++ Assets/Scripts/Blocks/PinkBlock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinkBlock : RedBlock {
+
+    [Header("Respawn")]
+    [SerializeField] private float respawnTime;
+    private Coroutine respawnRoutine;
+    private Vector3 basePosition;
+
+    protected override void Awake() {
+        base.Awake();
+        basePosition = transform.position;
+    }
+    
+    // -----------------------------------------------------------------------------------------------------------
+
+    public override void OnObjectGrab() {
+        base.OnObjectGrab();
+
+        if(respawnRoutine != null)
+            StopCoroutine(respawnRoutine);
+    }
+
+    public override void OnObjectRelease() {
+        base.OnObjectRelease();
+
+        if(respawnRoutine != null)
+            StopCoroutine(respawnRoutine);
+        respawnRoutine = StartCoroutine(Respawn());
+    }
+
+    // -----------------------------------------------------------------------------------------------------------
+
+    private IEnumerator Respawn() {
+        for(float i = 0; i < respawnTime; i += Time.deltaTime)
+            yield return null;
+        transform.position = basePosition;
+        transform.rotation = Quaternion.identity;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+}
