4b594b32a0c743de157bd4cf08aed64e764d5ece
Spark <sparkfire.ffs@gmail.com>
2021-05-22 18:29:59 -0500

    Set up basic main menu

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public void StartGame() {
+        GameManager.gm.LoadScene("Test Scene");
+    }
+
+    public void QuitGame() {
+#if UNITY_EDITOR
+        Debug.Log("Attempting to quit game, but cannot proceed in editor.");
+#endif
+        Application.Quit();
+    }
+
+}
