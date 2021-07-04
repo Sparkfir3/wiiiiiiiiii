3cf41a196ae3e55e6cd77db68b8e6d8f93390388
Spark <sparkfire.ffs@gmail.com>
2021-07-03 22:38:52 -0500

    Toyed around with wiimote-UI interactions, WIP

---
+++ Assets/Scripts/UI/CursorButtonClicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorButtonClicker : MonoBehaviour {
+
+    public GameObject test;
+
+    private void Update() {
+        if(InputManager.instance.mode != InputMode.Wiimote)
+            return;
+
+        test = InputManager.instance.SelectedObject(LayerMask.GetMask("UI"));
+
+        if(InputManager.instance.GetCommandDown(Command.SelectUI)) {
+
+        }
+    }
+
+}
